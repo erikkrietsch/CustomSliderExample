283fa83f69c0399f5ede34e03ef787008d49475c
Erik Krietsch <erik.krietsch@gmail.com>
2015-03-18 16:19:01 -0500

    Initial Commit

---
+++ CustomSliderExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSliderExample
+//
+//  Created by Erik Krietsch on 3/18/15.
+//  Copyright (c) 2015 Erik Krietsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
